1c161630a257af2aaa46b9c7b9ca7590b91740dc
Ada Loveless <xhorj@protonmail.com>
2022-05-17 16:50:49 +0200

    feat: added user context logic for sign up / sign out button

--- frontend/src/widgets/sections/NavigationMenu.jsx
+++ frontend/src/widgets/sections/NavigationMenu.jsx
@@ -1,16 +1,23 @@
+import { useUserContext } from "contexts/UserContext"
 import React from "react"
 import { Link } from "react-router-dom"
 
 const NavigationMenu = () => {
+  const { isUserLoggedIn } = useUserContext()
+
+  const displaySignUpOrSignOutLink = () => {
+    if (!isUserLoggedIn) {
+      return <Link to="connection">Sign Up</Link>
+    } else return <Link to="disconnection">Sign Out</Link>
+  }
+
   return (
     <nav>
       <ul className="flex justify-between">
         <li className="mr-3">
           <Link to="/">Home</Link>
         </li>
-        <li className="ml-3">
-          <Link to="connection">Sign Up</Link>
-        </li>
+        <li className="ml-3">{displaySignUpOrSignOutLink()}</li>
       </ul>
     </nav>
   )
