de916427540f405d11d0379b3ee887b67370cafe
Ada Loveless <xhorj@protonmail.com>
2022-05-18 09:29:57 +0200

    feat: implemented api call to movie db api

--- frontend/src/widgets/forms/SearchBar.jsx
+++ frontend/src/widgets/forms/SearchBar.jsx
@@ -1,14 +1,27 @@
-import React from "react"
+import React, { useState } from "react"
 import SearchButton from "widgets/buttons/SearchButton"
 
 const SearchBar = () => {
-  const submit = () => {
-    console.log("submit")
+  const [results, setResults] = useState(null)
+
+  const submit = async (query) => {
+    try {
+      const APIKEY = process.env.REACT_APP_API_KEY
+      const response = await fetch(
+        `https://api.themoviedb.org/3/search/movie?api_key=${APIKEY}&query=${query}`
+      )
+      if (response.status >= 200 && response.status <= 299) {
+        const data = await response.json()
+        setResults(data)
+      } else throw new Error(response.statusText)
+    } catch (err) {
+      setResults(null)
+    }
   }
 
   const handleKeyDown = (e) => {
     if (e.key === "Enter") {
-      submit()
+      submit(e.target.value)
     }
   }
 
