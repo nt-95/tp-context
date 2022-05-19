348bd8b771da41f4ab21bb914bdb1d5d3e39d974
Ada Loveless <xhorj@protonmail.com>
2022-05-19 14:54:51 +0200

    fix: height of layout

--- frontend/src/layouts/ContentLayout.jsx
+++ frontend/src/layouts/ContentLayout.jsx
@@ -1,7 +1,9 @@
 import React from "react"
 
 const ContentLayout = ({ children }) => {
-  return <div className="flex justify-center h-87%">{children}</div>
+  return (
+    <div className="flex justify-center items-center h-[87%]">{children}</div>
+  )
 }
 
 export default ContentLayout
--- frontend/src/layouts/HeaderLayout.jsx
+++ frontend/src/layouts/HeaderLayout.jsx
@@ -1,7 +1,7 @@
 import React from "react"
 
 const HeaderLayout = ({ children }) => {
-  return <div className="bg-black text-white p-4 h-10%">{children}</div>
+  return <div className="bg-black text-white p-4 h-[10%]">{children}</div>
 }
 
 export default HeaderLayout
