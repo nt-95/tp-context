e849072f6d4eea02ebfea21c84c968c9477cc592
Ada Loveless <xhorj@protonmail.com>
2022-05-19 14:20:45 +0200

    fix: height of header and content layouts

--- frontend/src/layouts/ContentLayout.jsx
+++ frontend/src/layouts/ContentLayout.jsx
@@ -1,7 +1,7 @@
 import React from "react"
 
 const ContentLayout = ({ children }) => {
-  return <div className="flex justify-center">{children}</div>
+  return <div className="flex justify-center h-87%">{children}</div>
 }
 
 export default ContentLayout
