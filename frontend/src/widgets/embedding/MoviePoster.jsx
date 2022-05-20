b750d70a70322c52660e756cb9582c3bf880c05f
Ada Loveless <xhorj@protonmail.com>
2022-05-20 12:12:41 +0200

    refactor: simplified movie poster return

--- frontend/src/widgets/embedding/MoviePoster.jsx
+++ frontend/src/widgets/embedding/MoviePoster.jsx
@@ -2,20 +2,17 @@ import React from "react"
 import placeholderPath from "assets/png/movie-poster-placeholder.png"
 
 const MoviePoster = ({ movie }) => {
-  const imageURL = `https://image.tmdb.org/t/p/w500/${movie.poster_path}`
-  if (!movie.poster_path) {
-    return (
-      <img
-        className="object-scale-down max-w-[90%] h-full"
-        src={placeholderPath}
-        alt="poster placeholder"
-      />
-    )
+  const getImagePath = () => {
+    if (!movie.poster_path) {
+      return placeholderPath
+    }
+    return `https://image.tmdb.org/t/p/w500/${movie.poster_path}`
   }
+
   return (
     <img
       className="object-scale-down max-w-[90%] h-full"
-      src={imageURL}
+      src={getImagePath()}
       alt="movie poster"
     />
   )
