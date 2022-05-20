c0d346ce17818d3e053bc11a69ea01163bca7659
Ada Loveless <xhorj@protonmail.com>
2022-05-20 19:44:53 +0200

    fix: movie page css with form

--- frontend/src/pages/MoviePage.jsx
+++ frontend/src/pages/MoviePage.jsx
@@ -15,16 +15,18 @@ const MoviePage = () => {
   }
   return (
     <PageLayout>
-      <div className="p-4 mx-4 text-white h-full w-full overflow-auto md:flex md:flex-wrap">
-        <MoviePosterWideLayout>
-          <MoviePoster movie={currentMovie} />
-        </MoviePosterWideLayout>
-        <section className="md:ml-2 md:max-w-[55%] lg:w-[65%]">
-          <p className="text-xl mt-6 md:mt-0 mb-6">{currentMovie.title}</p>
-          <p className="mt-2">{currentMovie.release_date}</p>
-          <p className="mt-2 mb-4">{`Note: ${currentMovie.vote_average}`}</p>
-          <p className="mt-2 mb-10 text-zinc-300">{currentMovie.overview}</p>
-          <ReviewArea />
+      <div className="p-4 mx-4 text-white h-full w-full overflow-auto ">
+        <section className="md:flex">
+          <MoviePosterWideLayout>
+            <MoviePoster movie={currentMovie} />
+          </MoviePosterWideLayout>
+          <section className="md:ml-2 md:max-w-[55%] lg:max-w-[65%] xl:max-w-[70%] flex flex-col justify-between">
+            <p className="text-xl mt-6 md:mt-0 mb-6">{currentMovie.title}</p>
+            <p className="mt-2">{currentMovie.release_date}</p>
+            <p className="mt-2 mb-4">{`Note: ${currentMovie.vote_average}`}</p>
+            <p className="mt-2 mb-10 text-zinc-300">{currentMovie.overview}</p>
+            <ReviewArea />
+          </section>
         </section>
         <ReviewListSection />
       </div>
