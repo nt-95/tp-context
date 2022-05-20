7304cc356c3259860add36cfefde53e90348aced
Ada Loveless <xhorj@protonmail.com>
2022-05-20 12:11:59 +0200

    fix: content layout to height 90%

--- frontend/src/layouts/ContentLayout.jsx
+++ frontend/src/layouts/ContentLayout.jsx
@@ -2,7 +2,7 @@ import React from "react"
 
 const ContentLayout = ({ children }) => {
   return (
-    <div className="flex justify-center items-center h-[87%]">{children}</div>
+    <div className="flex justify-center items-center h-[90%]">{children}</div>
   )
 }
 
